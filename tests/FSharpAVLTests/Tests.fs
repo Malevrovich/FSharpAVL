46928be1f2e089473f2b54fe70c87af67e82c9f9
malevrovich <dddccc20000@mail.ru>
2024-10-22 00:41:51 +0300

    added some unit tests

--- tests/FSharpAVLTests/Tests.fs
+++ tests/FSharpAVLTests/Tests.fs
@@ -10,7 +10,7 @@ open System.Collections.Generic
 let ``My test`` () = Assert.True(true)
 
 [<Fact>]
-let ``AccessTest`` () =
+let ``Access test`` () =
     Assert.True(
         AVLTree.empty
         |> AVLTree.add 1 2
@@ -23,11 +23,33 @@ let ``AccessTest`` () =
     )
 
 [<Fact>]
-let ``EnumerateTest`` () =
+let ``Enumerate test`` () =
     Assert.True(
-        Seq.ofList [ 0..4 ]
-        |> Seq.fold (fun tree el -> tree |> AVLTree.add el el) AVLTree.empty
+        AVLTree.ofItems (List.zip [ 0..4 ] [ 0..4 ])
         |> Seq.map (fun (k, v) -> k)
         |> List.ofSeq =
             [ 0..4 ]
+    )
+
+let shuffle seq =
+    let array = seq |> Seq.toArray
+    let random = Random()
+
+    for i in 0 .. array.Length - 1 do
+        let j = random.Next(i, array.Length)
+        let pom = array.[i]
+        array.[i] <- array.[j]
+        array.[j] <- pom
+
+    array |> Array.toSeq
+
+[<Fact>]
+let ``Storage test`` () =
+    let init = Seq.initInfinite (fun i -> i, i * i) |> Seq.take 10000
+    let tree = init |> shuffle |> AVLTree.ofItems
+
+    Assert.True(
+        init
+        |> Seq.map (fun (k, v) -> tree |> AVLTree.tryGet k = Some v)
+        |> Seq.reduce (&&)
     )
